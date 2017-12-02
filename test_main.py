c362f7e9ee8b74013d91df3b93363202e95d97ee
ant0x00 <ant0x00@163.com>
2017-12-02 22:56:13 +0800

    gogogo

--- test_main.py
+++ test_main.py
@@ -5,21 +5,10 @@ import pytest
 from appium import webdriver
 
 import config
+from base_test_case import *
 
 
-class TestSimpleAndroid():
-    @pytest.fixture(scope="function")
-    def driver(self,request):
-        print("start...")
-        desired_caps = config.DesiredCaps.desired_caps
-        driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-        def tear_down():
-            # end the session
-            print("tear down...")
-            driver.quit()
-        request.addfinalizer(tear_down)
-        return driver
+class TestSimpleAndroid(BaseTestCase):
 
     def test_find_elements(self, driver):
         print("test case...")
