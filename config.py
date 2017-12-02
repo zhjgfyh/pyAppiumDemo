30bc60ff25d58008f5f18e9cec3113d777b32292
ant0x00 <ant0x00@163.com>
2017-12-02 22:36:54 +0800

    gogogo

--- config.py
+++ config.py
@@ -4,14 +4,14 @@
 class DesiredCaps:
     desired_caps={}
     desired_caps['platformName'] = 'Android' #平台
-    desired_caps['platformVersion'] = '5.1.1' #android版本
-    desired_caps['deviceName'] = 'MI_4' #手机devicename
-    desired_caps['udid'] = '11e898e2' #手机udid
+    desired_caps['platformVersion'] = '4.3' #android版本
+    desired_caps['deviceName'] = '192.168.56.101:5555' #手机devicename
+    # desired_caps['udid'] = '11e898e2' #手机udid
     # desired_caps['app'] = PATH(
     #     '../../../sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk'
     # )
-    desired_caps['appPackage'] = 'com.aoyou.android' #包名
-    desired_caps['appActivity'] = '.view.common.MainActivity'#launcher activity
-    desired_caps['appWaitActivity'] = '.view.common.MainActivity'
+    desired_caps['appPackage'] = 'com.manboker.headportrait' #包名
+    desired_caps['appActivity'] = '.activities.SplashActivity'#launcher activity
+    desired_caps['appWaitActivity'] = '.activities.EntryActivity'
     desired_caps['deviceReadyTimeout'] = 100
     desired_caps['newCommandTimeout'] = 120
