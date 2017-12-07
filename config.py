01a8a62480abe567567974df1e3c40cecb033c59
wanglong <ant0x00@163.com>
2017-12-07 18:48:29 +0800

    f

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 #encoding: utf-8
 
-
+samsung_s7 = {
+    'platformName':'Android',
+    'version' : '6.0.1',
+    'deviceName':'SM_G9350',
+    'udid':'6a1ca77c'
+}
 class DesiredCaps:
     desired_caps={}
-    desired_caps['platformName'] = 'Android' #平台
-    desired_caps['platformVersion'] = '4.3' #android版本
-    desired_caps['deviceName'] = '192.168.56.101:5555' #手机devicename
-    # desired_caps['udid'] = '11e898e2' #手机udid
+    desired_caps = samsung_s7
+    # for value in samsung_s7:
+    #     desired_caps[value] = samsung_s7[value]
     # desired_caps['app'] = PATH(
     #     '../../../sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk'
     # )
-    desired_caps['appPackage'] = 'com.manboker.headportrait' #包名
-    desired_caps['appActivity'] = '.activities.SplashActivity'#launcher activity
-    desired_caps['appWaitActivity'] = '.activities.EntryActivity'
+    desired_caps['appPackage'] = 'com.aoyou.android' #包名
+    desired_caps['appActivity'] = '.view.common.MainActivity'#launcher activity
+    desired_caps['appWaitActivity'] = '.view.common.MainActivity'
     desired_caps['deviceReadyTimeout'] = 100
     desired_caps['newCommandTimeout'] = 120
--- test_main.py
+++ test_main.py
@@ -12,11 +12,11 @@ import time
 class TestSimpleAndroid(object):
 
     def test_find_elements(self, driver):
-        #driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-        print("test case...")
-        el = driver.find_element_by_class_name('android.view.View')
-        el.click()
+        # driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        print("test case1...")
+        assert driver is not None
 
-        start_btn = driver.find_element_by_id('com.manboker.headportrait:id/clsd_style_btn')
-        #camera = driver.find_element_by_id('com.manboker.headportrait:id/comics_main_top_view_to_camera_iv')
-        assert start_btn is not None
+    def test_find_elements_sec(self, driver):
+        # driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        print("test case2...")
+        assert driver is not None
